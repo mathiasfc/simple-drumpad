8c886d36c977fe0c5184d1c66e8b60e6a51bd66f
Mathias <mathias.castro@pmweb.com.br>
2018-10-03 12:59:39 -0300

    Config Panel structure

--- simple-drumpad-app/src/App.vue
+++ simple-drumpad-app/src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="drumpad" class="container">
+    <ConfigPanel />
+    
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        //HelloWorld
+    }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+    background: black;
+}
+
+.container {
+    width: 80%;
+    border: 1px solid grey;
+    height: 380px;
+    margin: 0 auto;
+    background: #000000;
+    background: -webkit-linear-gradient(to right, #434343, #000000);
+    background: linear-gradient(to right, #434343, #000000);
 }
 </style>
---
+++ simple-drumpad-app/src/components/ConfigPanel.vue
@@ -0,0 +1,58 @@
+<template>
+<div id="config-panel">
+    <span class="custom-dropdown">
+     <select>    
+            <option>Hip Hop</option>
+            <option>Jazz</option>  
+            <option>Reaggeton</option>
+     </select> 
+     </span>
+    <input type="button" class="btnControls" value="Controls" id="btnControls" />
+</div>
+</template>
+
+<script>
+export default {
+    name: 'ConfigPanel',
+    props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style lang="scss" scoped>
+#config-panel {
+    height: 60px;
+
+    .custom-dropdown {
+        position: relative;
+        display: inline-block;
+        vertical-align: middle;
+        margin: 5px;
+        margin-left: 14px;
+
+        &:before,
+        &:after {
+            content: "";
+            position: absolute;
+            pointer-events: none;
+        }
+
+        select {
+            cursor: pointer;
+            background-color: #5f5f5f;
+            color: white;
+            font-size: inherit;
+            padding: .5em;
+            padding-right: 2.5em;
+            border: 0;
+            margin: 0;
+            border-radius: 3px;
+            text-indent: 0.01px;
+            text-overflow: '';
+            -webkit-appearance: button;
+            /* hide default arrow in chrome OSX */
+        }
+    }
+}
+</style>
