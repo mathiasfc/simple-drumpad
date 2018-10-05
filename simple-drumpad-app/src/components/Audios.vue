4e19020ef8446c28da50d6a2e0fa183c21da04b9
Mathias <mathias.castro@pmweb.com.br>
2018-10-05 19:04:53 -0300

    preload events

--- simple-drumpad-app/src/components/Audios.vue
+++ simple-drumpad-app/src/components/Audios.vue
@@ -13,40 +13,66 @@
 </template>
 
 <script>
-import { Bus } from '../main.js';
+import { Bus } from "../main.js";
 
 export default {
-  name: 'Audios',
+  name: "Audios",
   data() {
     return {
+      audiosLoaded: [],
+      audiosReady: false,
       audios: [
         {
           key: 71,
-          name: 'snare',
-          src: 'http://hpanagramizer.freeoda.com/beats/HipHop/Claps/C - RnB.wav'
+          name: "snare",
+          src: "http://hpanagramizer.freeoda.com/beats/HipHop/Claps/C - RnB.wav"
         },
         {
           key: 72,
-          name: 'clap',
-          src: 'http://hpanagramizer.freeoda.com/beats/HipHop/Kick/K - Hot.wav'
+          name: "clap",
+          src: "http://hpanagramizer.freeoda.com/beats/HipHop/Kick/K - Hot.wav"
         }
       ]
     };
   },
   created() {
-    Bus.$on('playAudio', keydown => {
+    this.preLoadAudios();
+
+    Bus.$on("playAudio", keydown => {
       this.playSoundKey(keydown.keyCode);
     });
   },
   methods: {
     playSoundKey(key) {
-      let audio = this.audios.find(obj => {
-        return obj.key === key;
+      if (this.audiosReady) {
+        let audio = this.audios.find(obj => {
+          return obj.key === key;
+        });
+        if (audio) {
+          let sound = document.createElement("audio");
+          sound.src = audio.src;
+          sound.play();
+        }
+      }
+    },
+    preLoadAudios() {
+      this.audios.forEach(element => {
+        var audio = new Audio();
+        var objAudio = {
+          audio: audio,
+          key: element.key
+        };
+        // once this file loads, it will call loadedAudio()
+        // the file will be kept by the browser as cache
+        audio.src = element.src;
+        audio.addEventListener("canplaythrough", this.loadedAudio(objAudio), false);
       });
-      if (audio) {
-        let sound = document.createElement('audio');
-        sound.src = audio.src;
-        sound.play();
+    },
+    loadedAudio(objAudio) {
+      this.audiosLoaded.push(objAudio);
+      if (this.audiosLoaded.length == this.audios.length) {
+        this.audiosReady = true;
+        console.warn("All audios loaded");
       }
     }
   }
